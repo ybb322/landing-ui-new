329d9a1751514f73ef15fd3edd05a25c2636175b
ybb322 <masalevich98@gmail.com>
2022-01-14 14:28:07 +0300

    - svg's are now png's - white gradient is now set in % - pt-default / pb-default classes - deleted; - section tag now has pt, pb 120px, instead of mt, mb 120px;

--- scripts/brandsSlider.js
+++ scripts/brandsSlider.js
@@ -1,9 +1,12 @@
-export let brandsSlider = $(".brands__items").slick({
-  autoplay: true,
-  autoplaySpeed: 7000,
-  infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  swipeToSlide: true,
-  arrows: false,
+export let brandsSlider = $(document).ready(function () {
+  $(".brands__items").slick({
+    initialSlide: 0,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    swipeToSlide: true,
+  });
 });
--- scripts/reviewsSlider.js
+++ scripts/reviewsSlider.js
@@ -1,9 +1,11 @@
-export let reviewsSlider = $(".reviews__items").slick({
-  autoplay: true,
-  autoplaySpeed: 3000,
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  swipeToSlide: true,
-  arrows: false,
+export let reviewsSlider = $(document).ready(function () {
+  $(".reviews__items").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    arrows: false,
+  });
 });
